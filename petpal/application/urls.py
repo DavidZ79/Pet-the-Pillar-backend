fe5e13e7cfa85d66a22eeaefc56bd15b4f54807c
David Zhou <david.m8f.z@gmail.com>
2023-11-15 05:17:05 -0500

    fixed application views

--- petpal/application/urls.py
+++ petpal/application/urls.py
@@ -5,6 +5,6 @@ app_name = 'application'
 
 urlpatterns = [
     path('', ApplicationCreateView.as_view(), name='application-create'),
-    path('<int:application_id>/update/', ApplicationCreateView.as_view(), name='update_application'),
-    path('list/', ApplicationListView.as_view(), name='application-list')
+    path('<int:application_id>', ApplicationView.as_view(), name='view_application'),
+    path('list/', ApplicationListView.as_view(), name='application-list'),
 ]
--- petpal/application/views.py
+++ petpal/application/views.py
@@ -1,78 +1,53 @@
-from rest_framework import status
-from rest_framework.response import Response
-from api.models import Application, Pet, PetShelter, PetSeeker
-from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter
+from rest_framework import permissions, filters
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, ListAPIView
+from django.shortcuts import get_object_or_404
+from api.models import Application, PetShelter, Pet
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import ApplicationSerializer
 
 
-class ApplicationCreateView(APIView):
+class ApplicationCreateView(CreateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
 
-    def post(self, request):  # Create
+    def perform_create(self, serializer):
+        pet_id = self.request.data.get('pet')
+        pet = get_object_or_404(Pet, id=pet_id)
+        if pet.status != 'Available':
+            raise serializer.ValidationError('This pet is not available for adoption.')
+        serializer.save(seeker=self.request.user)
 
-        # create application
-        serializer = ApplicationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def patch(self, request, application_id):  # Update
+class ApplicationView(RetrieveAPIView, UpdateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+    lookup_field = 'id'
 
-        def isShelterOrSeeker(user):  # helper function
-            if isinstance(user, PetShelter):
-                return 'shelter'
-            elif isinstance(user, PetSeeker):
-                return 'seeker'
-            else:
-                print("error: not shelter or seeker")
-                return 'error'
-
-        application = Application.objects.get(pk=application_id)
-        new_status = request.data.get('status')
-
-        user = request.user
-        # user is a shelter
-        if isShelterOrSeeker(user) == 'shelter':
-            if application.status == 'pending' and new_status in ['Accepted', 'Denied']:
-                application.status = new_status
-            else:
-                return Response({'error': 'Invalid status update'}, status=status.HTTP_400_BAD_REQUEST)
-        # user is a seeker
-        elif isShelterOrSeeker(user) == 'seeker':
-            if application.status in ['pending', 'accepted'] and new_status == 'withdrawn':
-                application.status = new_status
-            else:
-                return Response({'error': 'Invalid status update'}, status=status.HTTP_400_BAD_REQUEST)
-
-        else:
-            return Response({'error': 'Unauthorized'}, status=status.HTTP_403_FORBIDDEN)
-
-        serializer = ApplicationSerializer(application, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_update(self, serializer):
+        application = self.get_object()
+        user = self.request.user
+        serializer.save()
 
 
 class ApplicationListView(ListAPIView):
     serializer_class = ApplicationSerializer
-    permission_classes = [IsAuthenticated]
-    #filter_backends = [DjangoFilterBackend, OrderingFilter] causes crash
+    # filter_backends = [DjangoFilterBackend, filters.OrderingFilter] causes crashes
     filterset_fields = ['status']
     ordering_fields = ['timestamp', 'last_updated']
-    pagination_class = PageNumberPagination
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-        if isinstance(user, PetShelter):
-            return Application.objects.filter(pet__shelter=user)
+
+        if hasattr(user, 'petshelter'):
+            return Application.objects.filter(pet__shelter=user.petshelter)
+        elif hasattr(user, 'petseeker'):
+            return Application.objects.filter(seeker=user.petseeker)
         else:
-            # return an empty queryset
+            # user is not seeker or shelter
+            print("Error: user is not seeker or shelter")
             return Application.objects.none()
 
 
