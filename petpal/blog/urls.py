c48ee6d5770c6048dccb14f1f69aed52819fe6d4
YahyaElgabra <elgabrayahya@gmail.com>
2023-12-14 11:08:32 -0500

    channges?

--- petpal/blog/urls.py
+++ petpal/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', BlogCreateAPI.as_view(), name='blog-create'),
     path('<int:blog_id>', BlogAPI.as_view(), name='view-blog'),
     path('list/<int:shelter_id>', BlogListAPI.as_view(), name='blog-list'),
+    path('list2/<int:shelter_id>', BlogListAPI2.as_view(), name='blog-list'),
 ]
--- petpal/blog/views.py
+++ petpal/blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_object_or_404
 from django.http import Http404, JsonResponse
+from rest_framework.response import Response
 
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.exceptions import NotFound
+from rest_framework.pagination import PageNumberPagination
 
 from .serializer import BlogSerializer
 
@@ -37,10 +39,34 @@ class BlogAPI(RetrieveAPIView):
 
         result = self.serializer_class(instance=blog)
         return JsonResponse(result.data)
+    
+    def patch(self, request, *args, **kwargs):
+        blog = self.get_object()
+        blog.num_likes += 1  # Increment the likes
+        blog.save()
+        return Response(self.get_serializer(blog).data)
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 1
 class BlogListAPI(ListAPIView):
     serializer_class = BlogSerializer
+    permission_classes = [AllowAny] 
+
+    def get_queryset(self):
+        shelter_id = self.kwargs.get('shelter_id')
+        
+        try:
+            shelter = PetShelter.objects.get(pk=shelter_id)
+        except PetShelter.DoesNotExist:
+            raise NotFound('Shelter not found.')
+        
+        return Blog.objects.filter(shelter=shelter)
+    
+
+class BlogListAPI2(ListAPIView):
+    serializer_class = BlogSerializer
     permission_classes = [AllowAny]
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         shelter_id = self.kwargs.get('shelter_id')
